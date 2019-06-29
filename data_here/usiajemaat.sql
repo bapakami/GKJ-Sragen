54ff4b4335548de1faca7491ccf300a24f6f060d
dewanggappraja@github.com <dewangga.praja@tokopedia.com>
2019-06-29 19:50:55 +0700

    dynamic report usia, penghasilan dan pekerjaan

---
+++ data_here/usiajemaat.sql
@@ -0,0 +1,8 @@
+REPLACE INTO GKJKlasis.usiajemaat (idx,usia,deskripsi) VALUES 
+(1,'<12 th','anak (kurang 12 th)')
+,(2,'13-17 th','remaja (13-17 th)')
+,(3,'18-25 th','pemuda (18-25 th)')
+,(4,'26-40 th','dewasa muda/keluarga muda (26-40 th)')
+,(5,'41-60 th','dewasa (41-60 th)')
+,(6,'>60 th','adiyuswa (>60 th)')
+;
